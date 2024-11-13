79003ac923e3343d11ad1b6b666b5d61ae53e704
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-11-14 00:50:42 +0200

    dropdown overflow

--- app/components/Dropdown.js
+++ app/components/Dropdown.js
@@ -21,14 +21,27 @@ const Dropdown = () => {
   return (
     <div className="relative" ref={dropdownRef}>
       <div
-        className="border border-[#e6e6e6] rounded-[5px] p-1 w-full shadow-sm flex justify-between"
+        className="border border-[#e6e6e6] rounded-[5px] p-1 w-full shadow-sm flex justify-between hover:cursor-pointer"
         onClick={toggleDropdown}
       >
         <span>val</span>
         <KeyboardArrowDownOutlinedIcon sx={{ color: "#6c6e77" }} />
       </div>
       {isOpen && (
-        <ul className="absolute w-full bg-white border border-[#e6e6e6] border-t-0 rounded-[5px]">
+        <ul className="absolute w-full bg-white border border-[#e6e6e6] border-t-0 rounded-[5px] max-h-36 overflow-scroll">
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
+          <li className="p-1 hover:bg-[#f4f5f6]">option</li>
           <li className="p-1 hover:bg-[#f4f5f6]">option</li>
           <li className="p-1 hover:bg-[#f4f5f6]">option</li>
           <li className="p-1 hover:bg-[#f4f5f6]">option</li>
